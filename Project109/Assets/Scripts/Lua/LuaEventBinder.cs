75516bb69241c9ee988166f6c0952d353ff566e1
SIRYU <siryu2409@gmail.com>
2026-06-17 11:21:56 +0900

    [refactor] LuaEventBinder 최적화를 위한 C# Source Generator 도입 및 빌드 오류 수정
    - EventBinderGenerator.csproj의 Roslyn 분석기 의존성 버전을 Unity 6 사양(Roslyn 4.3.0)에 맞게 다운그레이드
    - EventBinderSourceGenerator.cs에 Assembly-CSharp 어셈블리만 코드가 생성되도록 필터링 로직 추가
    - EventBinderGenerator.dll.meta에 RoslynAnalyzer 및 SourceGenerator 레이블 명시 및 validateReferences 옵션 비활성화(0) 처리
    - LuaEventBinder.cs를 partial 메소드 기반으로 리팩토링하여 GC Alloc 0 Byte 기반의 정적 컴파일 바인딩 구조 안착 완료

--- Project109/Assets/Scripts/Lua/LuaEventBinder.cs
+++ Project109/Assets/Scripts/Lua/LuaEventBinder.cs
@@ -1,148 +1,19 @@
 using XLua;
 using System.Collections.Generic;
 
-public static class LuaEventBinder
+public static partial class LuaEventBinder
 {
     // =========================================================
     // 1. Character Events (전투 및 캐릭터 상태 관련)
     // =========================================================
-    public static void BindCharacterEvents(LuaTable luaTable, EventBus<ICharacterEvent> eventBus, List<object> activeProxies)
-    {
-        if (luaTable == null || eventBus == null) return;
-
-        // Damage Interfaces
-        BindCharacter<IOnBeforeDealDamage>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnBeforeTakeDamage>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnAfterDealDamage>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnAfterTakeDamage>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnBreakShield>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnShieldBroken>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnKill>(luaTable, eventBus, activeProxies);
-
-        // Heal Interfaces
-        BindCharacter<IOnBeforeGiveHeal>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnBeforeTakeHeal>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnAfterGiveHeal>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnAfterTakeHeal>(luaTable, eventBus, activeProxies);
-
-        // Shield Interfaces
-        BindCharacter<IOnBeforeGiveShield>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnBeforeTakeShield>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnAfterGiveShield>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnAfterTakeShield>(luaTable, eventBus, activeProxies);
-
-        // Stamina Interfaces
-        BindCharacter<IOnBeforeSpendStamina>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnAfterSpendStamina>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnBeforeTakeStamina>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnAfterTakeStamina>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnBeforeGiveStamina>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnAfterGiveStamina>(luaTable, eventBus, activeProxies);
-
-        // Movement Interfaces
-        BindCharacter<IOnBeforeMove>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnAfterMove>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnBeforeForcedMove>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnAfterForcedMove>(luaTable, eventBus, activeProxies);
-
-        // Core State Interfaces
-        BindCharacter<IOnBattleStart>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnBattleEnd>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnTurnStart>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnTurnEnd>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnDeath>(luaTable, eventBus, activeProxies);
-
-        // Card Interfaces
-        BindCharacter<IOnBeforeUseCard>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnAfterUseCard>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnTryUseCard>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnDiscardCard>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnDrawCard>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnExhaustCard>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnShuffleDeck>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnEraseCard>(luaTable, eventBus, activeProxies);
-
-        // Effect Interfaces
-        BindCharacter<IOnBeforeGiveEffect>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnBeforeTakeEffect>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnAfterGiveEffect>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnAfterTakeEffect>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnBeforeRemoveEffect>(luaTable, eventBus, activeProxies);
-        BindCharacter<IOnAfterRemoveEffect>(luaTable, eventBus, activeProxies);
-    }
-
-    public static void UnbindCharacterEvents(EventBus<ICharacterEvent> eventBus, List<object> activeProxies)
-    {
-        if (eventBus == null) return;
-
-        foreach (var proxy in activeProxies)
-        {
-            if (proxy == null) continue;
-            var interfaces = proxy.GetType().GetInterfaces();
-            foreach (var iface in interfaces)
-            {
-                if (typeof(ICharacterEvent).IsAssignableFrom(iface) && iface != typeof(ICharacterEvent))
-                {
-                    try
-                    {
-                        var removeMethod = eventBus.GetType().GetMethod("Remove").MakeGenericMethod(iface);
-                        removeMethod.Invoke(eventBus, new object[] { proxy });
-                    }
-                    catch (System.Exception e)
-                    {
-                        UnityEngine.Debug.LogError($"[LuaEventBinder] 캐릭터 이벤트 해제 실패 ({iface.Name}): {e.Message}");
-                    }
-                }
-            }
-        }
-    }
-
+    public static partial void BindCharacterEvents(LuaTable luaTable, EventBus<ICharacterEvent> eventBus, List<object> activeProxies);
+    public static partial void UnbindCharacterEvents(EventBus<ICharacterEvent> eventBus, List<object> activeProxies);
 
     // =========================================================
     // 2. Player Events (게임 진행, 덱, 재화 관련)
     // =========================================================
-    public static void BindPlayerEvents(LuaTable luaTable, EventBus<IPlayerEvent> eventBus, List<object> activeProxies)
-    {
-        if (luaTable == null || eventBus == null) return;
-
-        BindPlayer<IOnAddRelic>(luaTable, eventBus, activeProxies);
-        BindPlayer<IOnRemoveRelic>(luaTable, eventBus, activeProxies);
-        BindPlayer<IOnAddCard>(luaTable, eventBus, activeProxies);
-        BindPlayer<IOnRemoveCard>(luaTable, eventBus, activeProxies);
-        BindPlayer<IOnCardUpgrade>(luaTable, eventBus, activeProxies);
-        BindPlayer<IOnCardMasteryUpgrade>(luaTable, eventBus, activeProxies);
-        BindPlayer<IOnCardsRefreshed>(luaTable, eventBus, activeProxies);
-        BindPlayer<IOnAddGold>(luaTable, eventBus, activeProxies);
-        BindPlayer<IOnRemoveGold>(luaTable, eventBus, activeProxies);
-        BindPlayer<IOnAddMemorySharp>(luaTable, eventBus, activeProxies);
-        BindPlayer<IOnRemoveMemorySharp>(luaTable, eventBus, activeProxies);
-    }
-
-    public static void UnbindPlayerEvents(EventBus<IPlayerEvent> eventBus, List<object> activeProxies)
-    {
-        if (eventBus == null) return;
-
-        foreach (var proxy in activeProxies)
-        {
-            if (proxy == null) continue;
-            var interfaces = proxy.GetType().GetInterfaces();
-            foreach (var iface in interfaces)
-            {
-                if (typeof(IPlayerEvent).IsAssignableFrom(iface) && iface != typeof(IPlayerEvent))
-                {
-                    try
-                    {
-                        var removeMethod = eventBus.GetType().GetMethod("Remove").MakeGenericMethod(iface);
-                        removeMethod.Invoke(eventBus, new object[] { proxy });
-                    }
-                    catch (System.Exception e)
-                    {
-                        UnityEngine.Debug.LogError($"[LuaEventBinder] 플레이어 이벤트 해제 실패 ({iface.Name}): {e.Message}");
-                    }
-                }
-            }
-        }
-    }
+    public static partial void BindPlayerEvents(LuaTable luaTable, EventBus<IPlayerEvent> eventBus, List<object> activeProxies);
+    public static partial void UnbindPlayerEvents(EventBus<IPlayerEvent> eventBus, List<object> activeProxies);
 
 
     // =========================================================
